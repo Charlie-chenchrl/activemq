0b7c9560094026dc227a1625d9cdf1d40140a320
Charlie-chenchrl <chenchrl@amazon.com>
2023-06-05 22:26:34 -0700

    I want this to fail

--- activemq-broker/src/main/java/org/apache/activemq/broker/BrokerFactory.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/BrokerFactory.java
@@ -31,7 +31,7 @@ import org.apache.activemq.util.IOExceptionSupport;
  */
 public final class BrokerFactory {
 
-    I want this to fail #10!
+    I want this to fail #11!
 
     private static final FactoryFinder BROKER_FACTORY_HANDLER_FINDER = new FactoryFinder("META-INF/services/org/apache/activemq/broker/");
 
